0a8e45e5844b75b8596697d9177ca92dd338f0f1
n-turco <nicholas.turco@hotmail.com>
2026-03-06 14:00:54 -0500

    -added method to check if the table exists -added method to get the table schema -tested both methods

--- DataBaseManager/DataBaseServices.cs
+++ DataBaseManager/DataBaseServices.cs
@@ -3,43 +3,107 @@ using System.Collections.Generic;
 using System.Text;
 using System.Data.Common;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace DataBaseManager
 {
     class DataBaseServices
     {
+        public static string SourceTableName = "";
+        public static string DestinationTableName = "";
+        public static string SourceTableSchema = "";
+
+
+        //Method Name: DataBaseExists
+        //Description: This method extracts the database name from the connection string
+        //             It checks if it is null otherwise it first connects to the server
+        //             It then queries the system for a count of databases matching the name
+        //             It supplies the database with a parameter for the database name
+        //Parameters:  string connectionString - connection string to the server
+        //Returns:     bool - true if database name exists, otherwise false
         public static bool DataBaseExists(string connectionString)
         {
             
             SqlConnectionStringBuilder connString = new(connectionString);
-            string dataBase = connString.InitialCatalog; //extract the database name from the string
+            string dataBase = connString.InitialCatalog;        //extract the database name from the string
 
-            if (dataBase == null) {
-                return false;  //check if database name is null
+            if (string.IsNullOrEmpty(dataBase)) 
+            {
+                return false;               //check if database name is null
             }
 
-            connString.InitialCatalog = "master"; //connect to master to check existence
+            connString.InitialCatalog = "master";           //connect to master to check existence
 
-            using (SqlConnection conn = new(connString.ConnectionString))
+            using SqlConnection conn = new(connString.ConnectionString);
+            try
             {
+                string query = "SELECT COUNT(1) FROM sys.databases WHERE name = @dbName";
+                conn.Open();
+                using SqlCommand cmd = new(query, conn);
+                cmd.Parameters.Add("@dbName", SqlDbType.NVarChar).Value = dataBase;       //add database name as a parameter
+
+                int count = (int)cmd.ExecuteScalar();
+                return count > 0;                            // returns 1(true) if database if found otherwise 0 (false)
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool DatabaseTableExists(string dbTable, string connectionString)
+        {
+            if (string.IsNullOrEmpty(dbTable) || string.IsNullOrEmpty(connectionString))
+            {
+                return false;
+            } 
+            else
+            {
+                using SqlConnection connect = new(connectionString);   
                 try
                 {
-                    string query = "SELECT COUNT(1) FROM sys.databases WHERE name = @dbName";
-                    conn.Open();
-                    using (SqlCommand cmd = new SqlCommand(query, conn))
-                    {
-                        cmd.Parameters.AddWithValue("@dbName", dataBase); //add database name as a parameter
+                   
+                    string query = "SELECT COUNT(1) FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = @TableName";
+                    connect.Open();
+                    using SqlCommand cmd = new(query, connect);
+                    cmd.Parameters.Add("@TableName", SqlDbType.NVarChar).Value = dbTable;       //add table name as a parameter
 
-                        int count = (int)cmd.ExecuteScalar();
-                        return count > 0; // returns 1(true) if database if found otherwise 0 (false)
-                    }
-                }
-                catch (Exception ex) {
-                    Console.WriteLine(ex.ToString()); 
+                    int count = (int)cmd.ExecuteScalar();
+                    return count > 0;                                // returns 1(true) if database if found otherwise 0 (false)
+                } 
+                catch
+                {
+                    return false;
                 }
             }
-
-            return true;
         }
+        public static bool GetSchema(string dbTable, string connectionString, out DataTable schema)
+        {
+            schema = new DataTable();
+            if (string.IsNullOrEmpty(dbTable) || string.IsNullOrEmpty(connectionString))
+            {
+                return false;
+            }
+            else
+            {
+                using SqlConnection connect = new(connectionString);
+                string query = $"SELECT * FROM dbo.{dbTable}";             // query db for table schema
+                try
+                {
+                    connect.Open();
+
+                    using SqlCommand command = new(query, connect);
+                    using SqlDataReader reader = command.ExecuteReader(CommandBehavior.SchemaOnly);
+                    schema = reader.GetSchemaTable(); //store schema in DataTable
+                }
+                catch
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        
     }
 }
--- DataBaseManager/MainWindow.xaml.cs
+++ DataBaseManager/MainWindow.xaml.cs
@@ -5,6 +5,7 @@
   FIRST VERSION : 2026-03-02 
   DESCRIPTION   : Main UI for the Client. Handles interactions between User and databases
 */
+using System.Data;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,9 +19,6 @@ using System.Windows.Shapes;
 
 namespace DataBaseManager
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -30,60 +28,122 @@ namespace DataBaseManager
 
         private void CheckSource_OnClick(object sender, RoutedEventArgs e)
         {
-            //clear error messages
-            SServerErrLbl.Content = "";
-            SDatabaseErrLbl.Content = "";
 
-            //validate inputs
-            if (ConnectionString.ValidateServerName(SServerTxt.Text.Trim())) {
-                SServerErrLbl.Content = "Invalid Server Name";
+            ErrorLbl.Content = "";                      //clear error messages
+
+            if (ConnectionString.ValidateServerName(SServerTxt.Text.Trim())) {          //validate inputs
+                ErrorLbl.Content = "Invalid Server Name";
                 return;
             }
             if(ConnectionString.ValidateDatabaseName(SDatabaseTxt.Text.Trim()))
             {
-                SDatabaseErrLbl.Content = "Invalid Database name";
+                ErrorLbl.Content = "Invalid Database name";
                 return;
             }
-            ConnectionString.BuildSourceConnectionString(SServerTxt.Text.Trim(), SDatabaseTxt.Text.Trim());
+            ConnectionString.BuildSourceConnectionString(SServerTxt.Text.Trim(), SDatabaseTxt.Text.Trim());  //create connection string
+            SServerLbl.Content = $"Source Server: {SServerTxt.Text.Trim()}";            //display server  
+            SDatabaseLbl.Content = $"Source DB: {SDatabaseTxt.Text.Trim()}";            //display database
+   
+            ErrorLbl.Content = "";              //clear labels and text input
+            SServerTxt.Text = "";
+            SDatabaseTxt.Text = "";
         }
 
         private void CheckDestination_OnClick(object sender, RoutedEventArgs e)
         {
-            //clear error messages
-            DServerErrLbl.Content = "";
-            DDatabaseErrLbl.Content = "";
-
-            //validate inputs
-            if (ConnectionString.ValidateServerName(DServerTxt.Text.Trim()))
+            ErrorLbl.Content = "";           //clear error messages
+     
+            if (ConnectionString.ValidateServerName(DServerTxt.Text.Trim()))         //validate inputs
             {
-                DServerErrLbl.Content = "Invalid Server Name";
+                ErrorLbl.Content = "Invalid Server Name";
                 return;
             }
             if (ConnectionString.ValidateDatabaseName(DDatabaseTxt.Text.Trim()))
             {
-                DDatabaseErrLbl.Content = "Invalid Database name";
+                ErrorLbl.Content = "Invalid Database name";
                 return;
             }
             ConnectionString.BuildDestinationConnectionString(DServerTxt.Text.Trim(), DDatabaseTxt.Text.Trim());
+            DServerLbl.Content = $"Destination Server: {DServerTxt.Text.Trim()}\n";
+            DDatabaseLbl.Content = $"Destination DB: {DDatabaseTxt.Text.Trim()}";
+
+            ErrorLbl.Content = "";              //clear labels and text input
+            DDatabaseTxt.Text = "";
+            DServerTxt.Text = "";
         }
 
         private void TransferData_OnClick(object sender, RoutedEventArgs e)
         {
-            if (ConnectionString.ValidateTableName(DataTableTxt.Text.Trim()))
+            
+            if (ConnectionString.ValidateTableName(STableTxt.Text.Trim()))      //validate source table
             {
-                DataTableErrLbl.Content = "Specifiy Table name to transfer data.";
-            }
-            if (ConnectionString.SourceConnection != null) {
-                if (DataBaseServices.DataBaseExists(ConnectionString.SourceConnection))
-                {
-                    GeneralLbl.Content = "DataBase Exists";
-                } else
-                {
-                    MessageBox.Show("Database does not exists");
-                }                 
+                MessageBox.Show("Specify source table name");
+                return;
             } else
             {
+                DataBaseServices.SourceTableName = STableTxt.Text.Trim();
+            }
+            
+            if (ConnectionString.SourceConnection != null)                //check Source Connection strings exist
+                {           
+                if (DataBaseServices.DataBaseExists(ConnectionString.SourceConnection))       //check Source Databases exist
+                {    
+                    if (DataBaseServices.DatabaseTableExists(DataBaseServices.SourceTableName, ConnectionString.SourceConnection))   //check if table exists and get schema
+                    {
+                       if(DataBaseServices.GetSchema(DataBaseServices.SourceTableName, ConnectionString.SourceConnection, out DataTable sTbl))
+                        {
+                            //compare source schema to destination schema
+                            //if it is the same, copy data
+                            //if it is not abort
+                            //if table does not exist create one with source schema
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to get table schema.");
+                            return;
+                        }
+                    } 
+                    else
+                    {
+                        MessageBox.Show("Source table does not exist.");
+                        return;
+                    }
+                    
+                    //copy table data, if data fails to copy, rollback
+                } 
+                else
+                {
+                    MessageBox.Show("Database does not exists");
+                    return;
+                }                 
+            } 
+            else
+            {
                 MessageBox.Show("Invalid connection string.");
+                return;
+            }
+            if (ConnectionString.ValidateTableName(DTableTxt.Text.Trim()))      //validate destination table
+            {
+                MessageBox.Show("Specify destination table name");      
+                return;
+            }
+
+            if (ConnectionString.DestinationConnection != null)         //check DestinationConnection string exists
+            {
+                if (DataBaseServices.DataBaseExists(ConnectionString.DestinationConnection))    //check Destination Database exists
+                {
+                    //check if table exists
+
+                    //if table does not exist, create table in destination database
+                    //if table exists check schema, if it does not match notify user 
+                    //if table exists and schema matches source, copy data, if data fails to copy, rollback
+
+                }
+                else 
+                {
+                    MessageBox.Show("Database does not exists");
+                    return;
+                }
             }
         }
     }
